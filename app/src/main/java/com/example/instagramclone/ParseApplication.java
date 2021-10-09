e3d0b3ebf61545e6ace4f5ae4b7a517358663b9c
kcperidot <kcperidot@gmail.com>
2021-10-08 23:49:41 -0700

    Update: progress

--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -3,11 +3,14 @@ package com.example.instagramclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("MBbWtNVYRaIz6pioKVyo8okayEw4V7qUFrUNtvsr")
                 .clientKey("e3i7upCmXzTsixKy8URHtBoTBDztclpQqz9l2xr3")
